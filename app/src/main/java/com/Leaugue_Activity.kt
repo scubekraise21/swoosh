807aeb41052212760ca1dca762993788eb83020f
scubekraise21 <krishnadevk1729@gmail.com>
2020-04-27 14:31:26 +0530

    swoosh-1

---
+++ app/src/main/java/com/Leaugue_Activity.kt
@@ -0,0 +1,14 @@
+package com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Leaugue_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        setContentView(R.layout.activity_leaugue_)
+    }
+}
