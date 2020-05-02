50de82ea7ee2f2f8359bd5a06313405a27d83779
scubekraise21 <krishnadevk1729@gmail.com>
2020-05-02 13:11:15 +0530

    added-log

--- app/src/main/java/com/LeagueActivity.kt
+++ app/src/main/java/com/LeagueActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.swoosh.R
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
